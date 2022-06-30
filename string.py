369ae7c7fb0ca6884fb30ba2943e0a0459d552e4
Sharonanota <matakwamatakwa>
2022-06-30 16:58:41 +0300

    new changes

--- string.py
+++ string.py
@@ -1,4 +1,8 @@
 #Convert half of string into capital 
+from doctest import script_from_examples
+from numpy import array
+
+
 def full():
  a="siara"
  b=len(a)
@@ -47,14 +51,79 @@ print("He said, \"What's there?\"")
    
   
    
-full()   
-def pali(words):
-    y=words.split()
-    y[0],y[-1]=y[-1],y[0]
-    left=0
-    right=len(words)-1
-    while left<right:
-        left+=1
-        right-=1
+# full()   
+# def pali(words):
+#     y=words.split()
+#     y[0],y[-1]=y[-1],y[0]
+#     left=0
+#     right=len(words)-1
+#     while left<right:
+#         left+=1
+#         right-=1
     
-pali(input("Enter word:"))                
+# pali(input("Enter word:"))    
+# person="Elon mask"
+# reaction="cool"
+# print("wow," +person + "!" +reaction + "!")
+# print("wow, %s! %s!" %(person,reaction) )
+# print("wow, {}! {}!".format(person,reaction))
+# print(f"wow,{person}!{reaction}!")
+
+
+
+# A. donuts
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+
+# def donuts(count):
+#     if count<9:
+#         print("number of donuts is many")
+#     if count>10:
+#         w=str(count)
+#         print("number of donuts:"+w)
+# donuts(int(input("Enter number:")))   
+
+# a="I am AkiraChix"
+# y=a.split() 
+# start=[0]
+# end=len(a)-1
+# while start < end:
+#     y[start],y[end]=y[end],y[start]
+#     start+=1
+#     end-=1
+#     s=""
+#     z=(y.join(s))
+#     if z==a:
+#         print("palindrome")
+#     else:
+#         print("not")    
+        
+        
+def palindrome(x):
+    start=0
+    end=len(x)-1
+    result=x.split()
+    while start<end:
+        result[start],result[end]=result[end],result[start]
+        start +=1
+        end-=1
+        s="".join(result)
+        print(s)
+        if s==x:
+            print("This is  not a palindrome")
+        else:
+            print("This is a palindrome")
+        # break
+palindrome(input("Enter a word:"))   
+
+
+
+  
+    
+    
+
+           
